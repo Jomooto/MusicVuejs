ba3c03ba680645ab88514b9cbc290352debae31a
Josafat Morales <joex45125@gmail.com>
2021-03-17 06:22:47 -0600

    iniciando el curso

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Bienvenido al mundo de Vuejs ok'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
